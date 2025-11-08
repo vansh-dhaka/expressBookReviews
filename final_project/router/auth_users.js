fa2cd12b7c6c7c16b3fbb1c024e05886d3210d6e
vansh-dhaka <27vanshdhaka@gmail.com>
2025-11-08 09:38:17 -0500

    update and test the authenticated user routes

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,19 +6,19 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-    let userswithsamename = users.filter((user) => user.username == username);
+    let userswithsamename = users.filter((user) => user.username === username);
     if(userswithsamename.length > 0) return true;
     else return false;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
     // Filter the users array for any user with the same username and password
-    let validUsers = users.filter((user) => {
-        return (user.username == username && user.password == password);
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
     });
 
     // Return true if any valid user is found, otherwise false
-    if (validUsers.length > 0) {
+    if (validusers.length > 0) {
         return true;
     } else {
         return false;
@@ -48,16 +48,32 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  let username = req.session.authorization["username"];
-  let review = req.query.review;
-  let exsistingReview = books[req.params.isbn].review[username];
-  if( exsistingReview ){
-    exsistingReview = review;
-  }else{
-    books[req.params.isbn].review[username] = review;
-  }
-  return res.status(300).json({message: "Review updated."});
-});
+    let username = req.session.authorization["username"];
+    let review = req.query.review;
+    let isbn = req.params.isbn;
+  
+    // Ensure book exists
+    if (!books[isbn]) {
+      return res.status(404).json({ message: "Book not found." });
+    }
+  
+    // Initialize review object if needed
+    if (!books[isbn].reviews) {
+      books[isbn].reviews = {};
+    }
+  
+    books[isbn].reviews[username] = review;
+  
+    return res.status(200).json({ message: "Review updated." });
+  });
+  
+// Deleting a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    let isbn = req.params.isbn;
+    let username = req.session.authorization["username"];
+    delete books[isbn].reviews[username];
+    res.status(200).json({message: "Review deleted."})
+})
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
